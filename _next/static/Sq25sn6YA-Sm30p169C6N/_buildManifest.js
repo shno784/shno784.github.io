eb8e2eb3f0d36370e07c09bab75119596d711368
Joshua Ollivierre <82393008+shno784@users.noreply.github.com>
2024-12-05 14:46:59 +0000

    added static website

---
+++ _next/static/Sq25sn6YA-Sm30p169C6N/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/953-e20e236c909c4cd5.js","static/chunks/pages/index-fa2c0ca730a18973.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
